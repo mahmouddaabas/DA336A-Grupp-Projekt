caf7370b6de58a66a412f458df62da7fb04ff5b5
Mahmoud Daabas <77792515+mahe0406@users.noreply.github.com>
2021-03-22 08:49:27 +0100

    Added a main window.
    Test

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -1,12 +1,26 @@
 package view;
 
+import javax.swing.*;
+
 public class MainFrame {
 
+    JFrame window;
+
     public MainFrame() {
+
+        createMainWindow();
 
     }
 
     public void createMainWindow() {
-        
+        window = new JFrame();
+        window.setSize(800, 600);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Inga restriktioner på fönstret. Kan lägga överallt.
+        window.setLayout(null);
+        window.setTitle("Climb the Tower");
+        //Startar på mitten av skärmen.
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }
