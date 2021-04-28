87c9556002f7ec2545f667b20964d496415ca5dc
Anniietran <anniiettran@hotmail.com>
2021-04-28 20:21:12 +0200

    Changed enemy health

--- src/controller/LevelCreator.java
+++ src/controller/LevelCreator.java
@@ -36,23 +36,23 @@ public class LevelCreator {
      */
     private void create1To5() {
         Level level1 = new Level(1, new Addition2Numbers(1,9,0,1,9,0),
-                new RegularMonster(false, RegularMonsterType.Goblin,5,"You see a goblin standing right infront of you, it doesn't seem that strong.",
+                new RegularMonster(false, RegularMonsterType.Goblin,3,"You see a goblin standing right infront of you, it doesn't seem that strong.",
                         "Prepare to die human!"), 10);
 
         Level level2 = new Level(2, new AdditionManyNumbers(2,13,0,3),
-                new RegularMonster(false, RegularMonsterType.Skeleton, 7, "A skeleton stands in the way, defeat it!",
+                new RegularMonster(false, RegularMonsterType.Skeleton, 3, "A skeleton stands in the way, defeat it!",
                         "*bone cracks*"), 12);
 
         Level level3 = new Level(3, new Subtraction2Numbers(4, 16,0, 4, 16,0, false),
-                new RegularMonster(false, RegularMonsterType.Warden, 10, "You see a rather short warden upfront. Should you get any closer?",
+                new RegularMonster(false, RegularMonsterType.Warden, 3, "You see a rather short warden upfront. Should you get any closer?",
                         "Halt! Who goes there?"), 15);
 
         Level level4 = new Level(4, new Multiplication2Numbers(3.0, 11.0, 0, 3.0, 11.0, 0),
-                new RegularMonster(false, RegularMonsterType.Hobgoblin, 14, "The goblin infront of you seem stronger than the previous one. Though he does seem talkative.",
+                new RegularMonster(false, RegularMonsterType.Hobgoblin, 3, "The goblin infront of you seem stronger than the previous one. Though he does seem talkative.",
                         "---"), 18);
 
         Level boss1 = new Level(5, new DivisionFraction2Numbers(12, 120, 6, 12),
-                new Boss(true, BossType.Orc, 20, "You spot the biggest green skin ever! Should you turn back?",
+                new Boss(true, BossType.Orc, 5, "You spot the biggest green skin ever! Should you turn back?",
                         "You have come to the wrong place human!"), 20);
 
         levels.add(level1);
@@ -67,19 +67,19 @@ public class LevelCreator {
      */
     private void create6To10() {
         Level level6 = new Level(6, new AdditionManyNumbers(4,26,0,3),
-                new RegularMonster(false, RegularMonsterType.Dwarf, 8, "*Dwarf look*", "*Dwarf talk*"), 18);
+                new RegularMonster(false, RegularMonsterType.Dwarf, 3, "*Dwarf look*", "*Dwarf talk*"), 18);
 
         Level level7 = new Level(7, new Subtraction2Numbers(9, 27, 0, 9, 27, 0, false),
-                new RegularMonster(false, RegularMonsterType.Gargoyle, 12, "*Gargoyle look*", "*Dwarf talk*"), 21);
+                new RegularMonster(false, RegularMonsterType.Gargoyle, 3, "*Gargoyle look*", "*Dwarf talk*"), 21);
 
         Level level8 = new Level(8, new Multiplication2Numbers(6.0, 12.0, 1, 6.0, 12.0, 1),
-                new RegularMonster(false, RegularMonsterType.SkeletonWarrior, 15, "*SkellyWarrior look*", "*SkellyWarrior talk*"), 24);
+                new RegularMonster(false, RegularMonsterType.SkeletonWarrior, 3, "*SkellyWarrior look*", "*SkellyWarrior talk*"), 24);
 
         Level level9 = new Level(9, new DivisionFraction2Numbers(14, 140, 7, 14),
-                new RegularMonster(false, RegularMonsterType.RoyalGuard, 18, "*RoyalGuard look*", "*RoyalGuard talk*"), 24);
+                new RegularMonster(false, RegularMonsterType.RoyalGuard, 3, "*RoyalGuard look*", "*RoyalGuard talk*"), 24);
 
         Level boss2 = new Level(10, new DivisionDecimal2Numbers(18, 360, 2, 9, 18, 2, 2),
-                new Boss(true, BossType.Paladin, 25, "*Boss2 look*", "*Boss2 talk*"), 28);
+                new Boss(true, BossType.Paladin, 5, "*Boss2 look*", "*Boss2 talk*"), 28);
 
         levels.add(level6);
         levels.add(level7);
@@ -90,19 +90,19 @@ public class LevelCreator {
 
     private void create11To15() {
         Level level11 = new Level(11, new AdditionManyNumbers(4, 26, 0, 3),
-                new RegularMonster(false, RegularMonsterType.SnakeMan, 18, "*SnakeMan look*", "*SnakeMan talk*"), 24);
+                new RegularMonster(false, RegularMonsterType.SnakeMan, 3, "*SnakeMan look*", "*SnakeMan talk*"), 24);
 
         Level level12 = new Level(12, new Multiplication2Numbers(7.0, 13.0, 1, 6.0, 12.0, 1 ),
-                new RegularMonster(false, RegularMonsterType.WorkSmithDwarf, 20, "*Buff Dwarf looks at you*", "*Worksmith dwarf talk*" ), 25);
+                new RegularMonster(false, RegularMonsterType.WorkSmithDwarf, 3, "*Buff Dwarf looks at you*", "*Worksmith dwarf talk*" ), 25);
 
         Level level13 = new Level(13, new Subtraction2Numbers(10, 29, 0, 9, 29, 0, false),
-                new RegularMonster(false, RegularMonsterType.SkeletonMage, 20, "*Skeleton Mage looks at you*", "Skeleton Mage talk*"), 25);
+                new RegularMonster(false, RegularMonsterType.SkeletonMage, 3, "*Skeleton Mage looks at you*", "Skeleton Mage talk*"), 25);
 
         Level level14 = new Level(14, new Multiplication2Numbers(9.0, 15.0, 1, 7.0, 15.0, 1),
-                new RegularMonster(false, RegularMonsterType.BuddhaGuardian, 25, "*BuddhaGuardian looks at you*", "BuddhaGuardian talk*"), 28);
+                new RegularMonster(false, RegularMonsterType.BuddhaGuardian, 3, "*BuddhaGuardian looks at you*", "BuddhaGuardian talk*"), 28);
 
         Level boss3 = new Level(15, new DivisionDecimal2Numbers(20, 360, 2, 10, 20, 2, 2),
-                new Boss(true, BossType.MageWarlord, 30, "*Boss3 look", "Boss3 talk*"), 30);
+                new Boss(true, BossType.MageWarlord, 5, "*Boss3 look", "Boss3 talk*"), 30);
 
         levels.add(level11);
         levels.add(level12);
@@ -113,19 +113,19 @@ public class LevelCreator {
 
     private void create16To20() {
         Level level16 = new Level(16, new AdditionManyNumbers(4, 26,0, 3),
-                new RegularMonster(false, RegularMonsterType.Ghost, 20, "*Ghost looks at you", "*Ghost talk"), 25);
+                new RegularMonster(false, RegularMonsterType.Ghost, 3, "*Ghost looks at you", "*Ghost talk"), 25);
 
         Level level17 = new Level(17, new AdditionManyNumbers(4, 26,0, 3),
-                new RegularMonster(false, RegularMonsterType.RedGuardian, 20, "*RedGuardian looks at you", "*RedGuardian talk"), 25);
+                new RegularMonster(false, RegularMonsterType.RedGuardian, 3, "*RedGuardian looks at you", "*RedGuardian talk"), 25);
 
         Level level18 = new Level(18, new AdditionManyNumbers(4, 26,0, 3),
-                new RegularMonster(false, RegularMonsterType.SpearMaster, 20, "*Spear Master looks at you", "*Spear Master talk"), 25);
+                new RegularMonster(false, RegularMonsterType.SpearMaster, 3, "*Spear Master looks at you", "*Spear Master talk"), 25);
 
         Level level19 = new Level(19, new AdditionManyNumbers(4, 26,0, 3),
-                new RegularMonster(false, RegularMonsterType.MiniBoss, 20, "*MiniBoss looks at you", "*MiniBoss talk"), 25);
+                new RegularMonster(false, RegularMonsterType.MiniBoss, 3, "*MiniBoss looks at you", "*MiniBoss talk"), 25);
 
         Level boss4 = new Level(20, new AdditionManyNumbers(4, 26,0, 3),
-                new Boss(true, BossType.FinalBoss, 20, "*Keanu looks at you", "*keanu talk"), 25);
+                new Boss(true, BossType.FinalBoss, 5, "*Keanu looks at you", "*keanu talk"), 25);
 
         levels.add(level16);
         levels.add(level17);
