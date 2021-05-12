3b022c921bdf9dacfc9aa4bd86613e17627e626a
Vilgot Mattsson <vilgotmattsson@hotmail.com>
2021-05-13 00:22:21 +0200

    Update MusicPlayer.java

--- src/controller/MusicPlayer.java
+++ src/controller/MusicPlayer.java
@@ -15,26 +15,38 @@ import java.util.ArrayList;
  * This class provides the game with backgroundmusic which change depending on the scene that is currently displaying.
  */
 
-public class MusicPlayer { //behövs det en tråd?
+public class MusicPlayer {
     private Counter counter;
     private MainFrame mainFrame;
 
+    private ArrayList<File> files;
+
     private AudioInputStream audioInput;
     private Clip clip;
-
     private FloatControl volume;
-
-    private ArrayList<File> files;
 
     private boolean isPlaying;
     private boolean isMuted;
     private boolean isShop;
 
+    /**
+     * Constructor that provides the private constructor with file paths to all of the audio clips
+     * @param counter A counter object to be used to get information regarding current level and scene
+     */
     public MusicPlayer(Counter counter) {
         this("resources/soundTracks/soundMissile.wav","resources/soundTracks/soundCars.wav","resources/soundTracks/soundMissile.wav","resources/soundTracks/soundMissile.wav","");
         this.counter = counter;
     }
 
+    /**
+     * Private constructor which initializes the ArrayList files and passes the values of the parameters to the
+     * creation of multiple File objects
+     * @param pathMainMenu filepath to the audio to be played on the MainMenu-screen/Startup-screen
+     * @param pathRegularLvl filepath to the audio to be played on all of the levels with regular monsters
+     * @param pathBossLvl filepath to the audio to be played on all of the levels with bosses
+     * @param pathShop filepath to the audio to be played on the
+     * @param pathWinner filepath to the audio to be played when player reaches the winner-screen
+     */
     private MusicPlayer(String pathMainMenu, String pathRegularLvl, String pathBossLvl, String pathShop, String pathWinner) {
         files = new ArrayList<>();
         files.add(0, new File(pathMainMenu));
@@ -43,12 +55,22 @@ public class MusicPlayer { //behövs det en tråd?
         files.add(3, new File(pathShop));
         files.add(4, new File(pathWinner));
     }
+
+    /**
+     * Method used by GameLogic to provide this class with GameLogic's MainFrame object
+     * @param mainFrame the MainFrame object to be passed to this class' MainFrame variable
+     */
     public void setMainFrame (MainFrame mainFrame) {
         this.mainFrame = mainFrame;
     }
 
+    /**
+     * Method that first determines which audio clip to be played based on the currently displaying scene and then provides
+     * the playMusic-method with the filepath to the desired audio clip
+     */
     public void startMusic() {
         if(isShop) {
+            //shop scene
             playMusic(files.get(3));
         } else {
             switch (counter.getLevel()) {
@@ -73,7 +95,7 @@ public class MusicPlayer { //behövs det en tråd?
                 case 17:
                 case 18:
                 case 19:
-                    //reglevels
+                    //regular levels
                     playMusic(files.get(1));
                     break;
 
@@ -81,14 +103,19 @@ public class MusicPlayer { //behövs det en tråd?
                 case 10:
                 case 15:
                 case 20:
-                    //bosslevels
+                    //boss levels
                     playMusic(files.get(2));
                     break;
             }
         }
     }
 
-    public void playMusic(File fileToPlay) {
+    /**
+     * Private method that uses an AudioInputStream and a Clip object to play audio
+     * Checks if any audio is playing and in that case stops the music before playing the audio
+     * @param fileToPlay A File object with the filepath to the audio which is used to pass to the AudioInputStream and to be played
+     */
+    private void playMusic(File fileToPlay) {
         if(isPlaying) {
             stopMusic();
         }
@@ -105,14 +132,15 @@ public class MusicPlayer { //behövs det en tråd?
                 } else {
                     setPreferredVolume(-30f);
                 }
-
-
             } catch (Exception e) {
                 e.printStackTrace();
             }
     }
 
-    // ska endast anropas när GUI:t skiftar scen och möjligtvis endast av startMusic() och då ifall scene inte korrelerar med activeScene
+
+    /**
+     * Method that stops the audio if playing
+     */
     public void stopMusic() {
         if(isPlaying) {
             clip.stop();
@@ -120,22 +148,33 @@ public class MusicPlayer { //behövs det en tråd?
         }
     }
 
+    /**
+     * Private method that sets the volume of the audio
+     * @param preferredVolume the float value to be set as volume
+     */
     private void setPreferredVolume(float preferredVolume) {
         volume = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
         volume.setValue(preferredVolume);
     }
 
+    /**
+     * Method that turns the audio on/off depending on the value of boolean variable isMute
+     * and then notifies the MainFrame about the change of state
+     */
     public void audioOnOff() {
         if(isMuted) { //om muted så sätts ljudet på
             setPreferredVolume(-30f);
         } else { //annars mutas ljudet
             setPreferredVolume(-50000f);
         }
-        mainFrame.setAudioIcon(isMuted);
         isMuted = !isMuted;
+        mainFrame.setAudioIcon(isMuted);
     }
 
-    //anropas när spelaren går in i shopen
+    /**
+     * Method that sets a boolean value to the variable isShop
+     * @param shopActive the boolean value to set on isShop
+     */
     public void setShopActive(boolean shopActive) {
         isShop = shopActive;
     }
